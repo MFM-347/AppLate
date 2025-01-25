545348e500e264cb4d17f360b67da30005daaa62
Farhan Madni <madnifm347@gmail.com>
2025-01-25 12:56:48 +0000

    update UI

--- app/src/main/java/dev/mfm/app/MainActivity.java
+++ app/src/main/java/dev/mfm/app/MainActivity.java
@@ -3,28 +3,19 @@ package dev.mfm.app;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import com.google.android.material.color.DynamicColors;
 import dev.mfm.app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
-  private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    DynamicColors.applyToActivityIfAvailable(this);
-    binding = ActivityMainBinding.inflate(getLayoutInflater());
-    setContentView(binding.getRoot());
-    Switch darkModeSwitch = findViewById(R.id.darkModeSwitch);
-    darkModeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-      AppCompatDelegate.setDefaultNightMode(
-        isChecked
-          ? AppCompatDelegate.MODE_NIGHT_YES
-          : AppCompatDelegate.MODE_NIGHT_NO
-      );
-    });
-  }
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 
   @Override
   protected void onDestroy() {
